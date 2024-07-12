ca38b01af92692558626fab7b60b9c3f50a393a6
Haess Romani <hromaza@gmail.com>
2024-07-12 17:15:58 +0200

    Conditional rendering

--- src/index.js
+++ src/index.js
@@ -66,15 +66,19 @@ function Header() {
 }
 
 function Menu() {
+  const numPizzas = pizzaData.length
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza, index) => (
-          <Pizza key={index} pizzaObj={pizza} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza, index) => (
+            <Pizza key={index} pizzaObj={pizza} />
+          ))}
+        </ul>
+      )}
     </main>
   )
 }
@@ -93,9 +97,22 @@ function Pizza(props) {
 }
 
 function Footer() {
-  const hour = new Date().toLocaleDateString()
+  const hour = new Date().getHours()
+  const openHour = 12
+  const closeHour = 22
+  const isOpen = hour >= openHour && hour <= closeHour
+  // console.log(isOpen)
 
-  return <footer className="footer">{hour}. We're currently open!</footer>
+  return (
+    <footer className="footer">
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
+    </footer>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
