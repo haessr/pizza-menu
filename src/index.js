f79609dcf787121e55aa52f02ca17095355e0dd2
Haess Romani <hromaza@gmail.com>
2024-08-08 11:05:04 +0200

    React Fragments

--- src/index.js
+++ src/index.js
@@ -73,11 +73,18 @@ function Menu() {
       <h2>Our Menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza, index) => (
-            <Pizza key={index} pizzaObj={pizza} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious
+          </p>
+
+          <ul className="pizzas">
+            {pizzaData.map((pizza, index) => (
+              <Pizza key={index} pizzaObj={pizza} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
