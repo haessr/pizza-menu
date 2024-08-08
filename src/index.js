b42c3045a8272e0e139c19f9cd9935d0014f3398
Haess Romani <hromaza@gmail.com>
2024-08-08 10:31:06 +0200

    Conditional rendering with ternaries

--- src/index.js
+++ src/index.js
@@ -72,12 +72,14 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza, index) => (
             <Pizza key={index} pizzaObj={pizza} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
     </main>
   )
@@ -105,11 +107,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
       )}
     </footer>
   )
