965f90b5fa3d7d1fb3f6f54c374f6169f47891e6
Haess Romani <hromaza@gmail.com>
2024-08-08 11:11:20 +0200

    Setting classes and text conditionally

--- src/index.js
+++ src/index.js
@@ -93,15 +93,15 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null
+  // if (pizzaObj.soldOut) return null
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price + 3}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   )
@@ -112,7 +112,6 @@ function Footer() {
   const openHour = 12
   const closeHour = 22
   const isOpen = hour >= openHour && hour <= closeHour
-  // console.log(isOpen)
 
   return (
     <footer className="footer">
